d218376ef43b1ba37b40a17119f1fc9c4cfa1d3b
iteabang <iteabang@gmail.com>
2023-01-03 22:25:52 +0900

    [생성자 패턴] 싱글톤 패턴 기본형 예제 추가

--- src/main/java/kr/iteabang/Main.java
+++ src/main/java/kr/iteabang/Main.java
@@ -1,7 +1,11 @@
 package kr.iteabang;
 
+import kr.iteabang.constructor.Singleton1;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Singleton1 singleton1 = Singleton1.getInstance();
+        Singleton1 singleton2 = Singleton1.getInstance();
+        System.out.println(singleton1.equals(singleton2));
     }
 }
---
+++ src/main/java/kr/iteabang/constructor/Singleton1.java
@@ -0,0 +1,15 @@
+package kr.iteabang.constructor;
+
+public class Singleton1 {
+
+    private static Singleton1 instance = new Singleton1();
+
+    private Singleton1() {
+
+    }
+
+    public static Singleton1 getInstance() {
+        return instance;
+    }
+
+}
